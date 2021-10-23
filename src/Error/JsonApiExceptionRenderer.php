e10cfc45919c13647ffa101c022ae3dfe682fc66
rrd <rrd@1108.cc>
2021-10-23 11:22:04 +0200

    set more data for the response

--- src/Error/JsonApiExceptionRenderer.php
+++ src/Error/JsonApiExceptionRenderer.php
@@ -2,16 +2,26 @@
 
 namespace JsonApiException\Error;
 
+use Cake\Utility\Hash;
 use Cake\Error\ExceptionRenderer;
+use JsonApiException\Error\Exception\JsonApiException;
 
 class JsonApiExceptionRenderer extends ExceptionRenderer
 {
-    public function jsonApi($exception)
+    public function jsonApi(JsonApiException $error)
     {
         $response = $this->controller->getResponse();
+        $data = [
+            'message' => $error->getMessage(),
+            'url' =>  $this->controller->getRequest()->getRequestTarget(),
+            'line' => $error->getLine(),
+            'errorCount' => count(Hash::flatten($error->getRequestErrors())),
+            'errors' => $error->getRequestErrors(),
+        ];
+
         return $response
-            ->withStringBody(json_encode(['errors' => $exception->getRequestErrors()]))
-            ->withStatus(406)
+            ->withStringBody(json_encode($data))
+            ->withStatus($error->getCode())
             ->withType('application/json');
     }
 }
