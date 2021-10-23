f0506c539f9189251d051f01d23209024f5eff62
rrd <rrd@1108.cc>
2021-10-23 11:02:19 +0200

    unused code removed

--- src/Plugin.php
+++ src/Plugin.php
@@ -6,7 +6,6 @@ namespace JsonApiException;
 
 use Cake\Core\Configure;
 use Cake\Core\BasePlugin;
-use Cake\Http\MiddlewareQueue;
 use Cake\Core\PluginApplicationInterface;
 
 /**
@@ -26,18 +25,5 @@ class Plugin extends BasePlugin
     public function bootstrap(PluginApplicationInterface $app): void
     {
         Configure::write('Error.exceptionRenderer', 'JsonApiException\Error\JsonApiExceptionRenderer');
-    }
-
-    /**
-     * Add middleware for the plugin.
-     *
-     * @param \Cake\Http\MiddlewareQueue $middleware The middleware queue to update.
-     * @return \Cake\Http\MiddlewareQueue
-     */
-    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
-    {
-        // Add your middlewares here
-
-        return $middlewareQueue;
     }
 }
