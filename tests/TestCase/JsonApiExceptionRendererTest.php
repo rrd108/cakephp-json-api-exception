9818e840bcac3df58de67174342d5933253955ce
rrd <rrd@1108.cc>
2022-02-28 12:28:55 +0100

    test added

--- tests/TestCase/JsonApiExceptionRendererTest.php
+++ tests/TestCase/JsonApiExceptionRendererTest.php
@@ -36,6 +36,33 @@ class JsonApiExceptionRendererTest extends TestCase
         $this->assertEquals($this->validationError, $responseBody->errors->name->_empty);
     }
 
+    public function testJsonApiWithEntitiesError()
+    {
+        $firstEntity = new Entity();
+        $firstEntity->setError('name', ['_empty' => $this->validationError]);
+        $secondEntity = new Entity();
+        $secondEntity->setError('name', ['_empty' => $this->validationError]);
+        $entities = [$firstEntity, $secondEntity];
+        $exception = new JsonApiException($entities, $this->message);
+
+        $request = (new ServerRequest())
+            ->withParam('controller', 'Foo')
+            ->withParam('action', 'bar');
+        $exceptionRenderer = new JsonApiExceptionRenderer($exception, $request);
+
+        $response = $exceptionRenderer->render();
+
+        $this->assertEquals(400, $response->getStatusCode());
+        $this->assertEquals('application/json', $response->getType());
+
+        $responseBody = json_decode($response->__toString());
+        $this->assertEquals($this->message, $responseBody->message);
+        $this->assertEquals('/', $responseBody->url);
+        $this->assertEquals(2, $responseBody->errorCount);
+        $this->assertEquals($this->validationError, $responseBody->errors[0]->name->_empty);
+        $this->assertEquals($this->validationError, $responseBody->errors[1]->name->_empty);
+    }
+
     public function testJsonApiWithErrorCode()
     {
         $entity = new Entity();
